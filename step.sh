a5cb96e567cbd568e8f0facbc2a7a9ed70da416a
Nick Hammond <n.hammond@waracle.com>
2017-10-20 13:31:20 +0100

    Added options to script

--- step.sh
+++ step.sh
@@ -1,24 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+set -o pipefail
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-#  envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+if [[ "${is_debug}" == 'yes' ]]; then
+	set -x
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+echo
+echo "JIRA Config:"
+echo "- url: $jira_url"
+echo "- user: $jira_user"
+echo "- password: $(if [ ! -z ${jira_password+x} ]; then echo "****"; fi)"
 
-JIRA_ISSUE=`echo $BITRISE_GIT_BRANCH | egrep -o '[A-Z]+-[0-9]+'`
+# Required input validation
+if [[ "${jira_url}" == "" ]]; then
+	echo
+	echo "No jira_url provided as environment variable. Terminating..."
+	echo
+	exit 1
+fi
+
+if [[ "${jira_user}" == "" ]]; then
+	echo
+	echo "No jira_user provided as environment variable. Terminating..."
+	echo
+	exit 1
+fi
+
+if [[ "${jira_password}" == "" ]]; then
+	echo
+	echo "No jira_password provided as environment variable. Terminating..."
+	echo
+	exit 1
+fi
+
+
+echo
+echo "Extracting JIRA Issue number from branch: $BITRISE_GIT_BRANCH"
+
+JIRA_ISSUE="$(echo $BITRISE_GIT_BRANCH | egrep -o '[A-Z]+-[0-9]+')"
+
+echo "Found '$JIRA_ISSUE'"
 
 # If no issue number, abort
 if [ ! "$JIRA_ISSUE" ]; then
@@ -26,11 +48,16 @@ if [ ! "$JIRA_ISSUE" ]; then
 	exit 0
 fi
 
+echo "Adding $JIRA_ISSUE to env. vars"
 envman add --key JIRA_ISSUE --value $JIRA_ISSUE
 
-# Generate comment body
-
-export COMMENT_BODY="Build $BITRISE_BUILD_NUMBER at $BITRISE_BUILD_URL"
-echo $COMMENT_BODY
-
-curl -D- -u $JIRA_USER:$JIRA_PASSWORD -X POST -H "Content-Type: application/json" -d "{\"body\": \"$COMMENT_BODY\"}" https://$JIRA_HOST_PORT/rest/api/2/issue/$JIRA_ISSUE/comment
+res="$(curl --write-out %{response_code} --silent --output /dev/null -u $jira_user:$jira_password -X POST -H "Content-Type: application/json" -d "{\"body\": \"${jira_build_message//$'\n'/\n}\"}" https://${jira_url}/rest/api/2/issue/$JIRA_ISSUE/comment)"
+if test "$res" == "201"; then
+    echo
+    echo "--- Posted comment to jira successfully"
+    echo "---------------------------------------------------"
+    exit 0
+else
+   echo "the curl command failed with: $res"
+   exit 1
+fi
